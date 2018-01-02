997d1e15c85e2597de8ecda6de70f3eced532b26
Gábor Kükedi <redrick@gabor-macbook-air.local>
2018-01-02 21:33:26 +0100

    Convert to MVC design Pattern,

---
+++ Todoey/Data model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Data Model.swift
+//  Todoey
+//
+//  Created by Gábor Kükedi on 2018. 01. 02..
+//  Copyright © 2018. Gábor Kükedi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
